2b7ec04fb80d18b205f817897e354da2f41584a9
Damilola Oyeyipo <damilola@7am.io>
2024-08-03 20:09:15 +0100

    Add OpenApi configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,48 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
 import { configuration } from '@Config/index';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Environment } from '@Common/types/env.enum';
+import { VersioningType } from '@nestjs/common';
+import * as compression from 'compression';
+import helmet from 'helmet'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger:
+      process.env.NODE_ENV !== Environment.LOCAL
+        ? ['warn', 'error', 'fatal']
+        : undefined,
+  });
+
+  // Helmet - Security Middleware
+  app.use(helmet());
+
+  // API Version
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  // Cross-Origin Resource Sharing (CORS)
+  // This is set to generic to not disturb any frontend integrations or tests
+  const origin = '*';
+  app.enableCors({ origin });
+
+  // Compression - Reduce Response Size
+  app.use(compression());
+
+  // Swagger OpenAPI
+  const config = new DocumentBuilder()
+    .setTitle('Shortlet API')
+    .setDescription('OpenAPI swagger documentation for Shortlet backend')
+    .addTag('Countries', 'Operations related to countries external API')
+    .addTag('Health', 'Health checks for application, database and Core')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  // Listen to serve
   await app.listen(configuration().appPort);
 }
 bootstrap();
